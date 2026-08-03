6b8abf6253bedee5bf78a1bc9070d739981732f5
noahhhi <29367133+noahhhi@users.noreply.github.com>
2026-08-03 17:50:07 +0800

    fix: prevent temporary familiar duplication on rewind

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local CharacterEnhance = RegisterMod("character-enhance", 1)
 
-local VERSION = "1.20.0"
+local VERSION = "1.20.1"
 local DEFAULT_SETTINGS = {
     menuLanguage = "en",
     eveDamageMultiplier = true,
